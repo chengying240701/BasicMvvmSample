6df155fa684854bc17a46dde91d97104ecf5be7e
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-03 12:34:35 +0800

    feat: add SimpleViewModel into MainWindowViewModel

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,4 +5,8 @@ public class MainWindowViewModel : ViewModelBase
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+
+    // Add our SimpleViewModel.
+    // Note: We need at least a get-accessor for our Properties.
+    public SimpleViewModel SimpleViewModel { get; } = new SimpleViewModel();
 }
